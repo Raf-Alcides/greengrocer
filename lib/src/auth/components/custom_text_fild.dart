9b3c035ad3844956d6c90caf1dac5abd5eb61453
Rafael Alcides <devrafalcides@gmail.com>
2023-03-07 15:14:30 -0300

    add params cuntom text fild

--- lib/src/auth/components/custom_text_fild.dart
+++ lib/src/auth/components/custom_text_fild.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextFild extends StatefulWidget {
   final IconData icon;
   final String label;
   final bool isSecret;
-  
+  final List<TextInputFormatter>? inputFormatters;
+  final TextInputType? keyboardType;
 
-   const CustomTextFild({
-    Key? key,
-    this.isSecret = false,
-    required this.icon,
-    required this.label,
-  }) : super(key: key);
+  const CustomTextFild(
+      {Key? key,
+      this.isSecret = false,
+      required this.icon,
+      required this.label,
+      this.inputFormatters,
+      this.keyboardType})
+      : super(key: key);
 
   @override
   State<CustomTextFild> createState() => _CustomTextFildState();
 }
 
 class _CustomTextFildState extends State<CustomTextFild> {
-
   bool isObscure = false;
 
   @override
@@ -32,15 +35,21 @@ class _CustomTextFildState extends State<CustomTextFild> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: TextFormField(
+        keyboardType: widget.keyboardType,
+        inputFormatters: widget.inputFormatters,
         obscureText: isObscure,
         decoration: InputDecoration(
             prefixIcon: Icon(widget.icon),
-            suffixIcon: widget.isSecret ? IconButton(onPressed: () {
-              setState(() {
-                isObscure = !isObscure;
-              });
-            } , icon:  Icon( isObscure ? Icons.visibility : Icons.visibility_off)
-            ) : null,
+            suffixIcon: widget.isSecret
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    icon: Icon(
+                        isObscure ? Icons.visibility : Icons.visibility_off))
+                : null,
             labelText: widget.label,
             isDense: true,
             border:
