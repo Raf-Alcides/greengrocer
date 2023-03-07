a06c9e306166217f9c02a8b833d0c3ff20dba661
Rafael Alcides <devrafalcides@gmail.com>
2023-03-07 15:17:45 -0300

    crete registe page

---
+++ lib/src/auth/register_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/custom_elevated_button.dart';
+import 'package:greengrocer/src/auth/components/custom_text_fild.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class Register extends StatelessWidget {
+  Register({Key? key}) : super(key: key);
+
+  final cpfFormatter = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {'#':RegExp(r'[0-9]')}
+  );
+
+   final phoneFormatter = MaskTextInputFormatter(
+    mask: '(##) # ####-####',
+    filter: {'#':RegExp(r'[0-9]')}
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.green[900],
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(color: Colors.white, fontSize: 35),
+                      ),
+                    ),
+                  ),
+                  //* Formulario
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 40,
+                      horizontal: 32,
+                    ),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(45))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children:  [
+                        const CustomTextFild(
+                          icon: Icons.email,
+                          label: 'Email',
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        const CustomTextFild(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        const CustomTextFild(
+                          icon: Icons.person,
+                          label: 'Nome',
+                          keyboardType: TextInputType.name,
+                        ),
+                        CustomTextFild(
+                          icon: Icons.phone,
+                          label: 'Celular',
+                          inputFormatters: [phoneFormatter],
+                          keyboardType: TextInputType.phone,
+                        ),
+                        CustomTextFild(
+                          icon: Icons.file_copy,
+                          label: 'CPF',
+                          inputFormatters: [cpfFormatter],
+                          keyboardType: TextInputType.number,
+                        ),
+                        const CustomElevatedButton(name: 'Cadastrar Usuario')
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
