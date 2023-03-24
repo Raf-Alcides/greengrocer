086e302e1bc1ba50cde4fa38201affd446310a8a
Rafael Alcides <devrafalcides@gmail.com>
2023-03-24 19:31:32 -0300

    add card_tile

---
+++ lib/src/pages/home/components/item_tile.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/models/model_item.dart';
+import 'package:greengrocer/src/utils/utils_service.dart';
+
+class ItemTile extends StatelessWidget {
+  final ModelItem item;
+
+  ItemTile({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final UtilsService utilsService = UtilsService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          elevation: 3,
+          shadowColor: Colors.grey[300],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(child: Image.asset(item.img)),
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      utilsService.priceToCurrency(item.price),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.green[900]),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey[400],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                  color: Colors.green[900],
+                  borderRadius:
+                      const BorderRadius.only(topRight: Radius.circular(20))),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/src/utils/utils_service.dart
@@ -0,0 +1,7 @@
+import 'package:intl/intl.dart';
+class UtilsService {
+  String priceToCurrency(double price) {
+    NumberFormat numberFormat = NumberFormat.simpleCurrency(locale: 'pt_BR');
+    return numberFormat.format(price);
+  }
+}
